bee71a5fdf69fffad40b21674f946c8a4c0aa2f4
NieLamu <mxmxlty@gmail.com>
2019-04-23 01:18:41 +0800

    feat: add framework7-vue demo from official website

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- Main Framework7 App component where we pass Framework7 params -->
+  <f7-app :params="f7Params">
+    <f7-statusbar></f7-statusbar>
+    <f7-panel left cover resizable>
+      <f7-view url="/panel-left/" links-view=".view-main"></f7-view>
+    </f7-panel>
+    <f7-panel right reveal resizable>
+      <f7-view url="/panel-right/"></f7-view>
+    </f7-panel>
+    <!-- initial page is specified in routes.js -->
+    <f7-view url="/" :main="true" class="safe-areas" :master-detail-breakpoint="800"></f7-view>
+  </f7-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { f7App, f7Panel, f7View, f7Statusbar } from "framework7-vue";
+import routes from "./routes";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    f7App,
+    f7Panel,
+    f7View,
+    f7Statusbar
+  },
+  data() {
+    // Demo Theme
+    let theme = "auto";
+    if (document.location.search.indexOf("theme=") >= 0) {
+      theme = document.location.search.split("theme=")[1].split("&")[0];
+    }
+
+    return {
+      f7Params: {
+        name: "MI NOTE",
+        id: "io.github.NieLamu",
+        theme,
+        routes,
+        popup: {
+          closeOnEscape: true
+        },
+        sheet: {
+          closeOnEscape: true
+        },
+        popover: {
+          closeOnEscape: true
+        },
+        actions: {
+          closeOnEscape: true
+        }
+      }
+    };
+  },
+  mounted() {
+    this.$f7ready(f7 => {
+      console.log("s", f7);
+      f7.dialog.alert("Component mounted");
+    });
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/app.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import './registerServiceWorker'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.config.productionTip = false
+// Framework7 Library CSS Bundle
+import 'framework7/css/framework7.bundle.min.css'
+// Import Framework7 Bundle
+import Framework7 from 'framework7/framework7.esm.bundle'
+// // Import Framework7-Vue with all components
+// import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle'
+// Import Framework7-Vue, import components you need in your components
+import Framework7Vue from 'framework7-vue'
+// Init plugin and register all components
+Framework7.use(Framework7Vue)
 
+Vue.config.productionTip = true
+Vue.use(VueAxios, axios)
+
+// Init Vue App
 new Vue({
   store,
   render: h => h(App)
